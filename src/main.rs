378c4ea24afab9b5104fd7d4fff908cd85c0fea4
Nocthir <nocthir@proton.me>
2025-09-07 18:45:35 +0200

    bevy: Worgen Plugin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(WorgenPlugin)
+        .run();
+}
+
+pub struct WorgenPlugin;
+
+impl Plugin for WorgenPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, start_info);
+    }
+}
+
+fn start_info() {
+    info!("Hello, Worgen!");
 }
